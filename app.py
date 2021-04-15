730f71fff04f83ae31c2caccce118e1dcdad1f8c
Masoud-Ghasemian <53497388+Masoud-Ghasemian@users.noreply.github.com>
2021-04-15 01:52:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import numpy as np
 
 
 # loading the trained model
-model_path = "/content/drive/MyDrive/Colab Notebooks/Deployment/Streamlit/model.pkl"
-scaler_path = "/content/drive/MyDrive/Colab Notebooks/Deployment/Streamlit/scaler.pkl"
+model_path = "https://github.com/Masoud-Ghasemian/Heart-Stroke-Prediction/model.pkl"
+scaler_path = "https://github.com/Masoud-Ghasemian/Heart-Stroke-Prediction/scaler.pkl"
 model = pickle.load(open(model_path, 'rb'))
 scaler = pickle.load(open(scaler_path, 'rb'))
 
